ac47772823cecf1072c6f440fbe3242bc6f23440
Pedro Javier Nicolás Zamora <pedrojavier.nz@gmail.com>
2020-02-19 14:39:12 +0100

    Init java project

---
+++ src/app/App.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class App {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
